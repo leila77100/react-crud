2193aa09067150709cf222019335da1ddeb2a5ab
Leila <72547745+leila77100@users.noreply.github.com>
2024-04-08 12:21:52 +0200

    Update firebaseConfig.js

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -2,17 +2,17 @@ import firebase from 'firebase';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAZkqj-cesxrAy9uPXZYvFk5OY6U1FwCN8",
-    authDomain: "react-video-auth-639f2.firebaseapp.com",
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 
     // paramètres utiles au CRUD=> variable disponible dans les paramètre généraux de firebase lors de la créationn de l'appli dessus 
 
-    databaseURL: "https://react-video-auth-639f2-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "react-video-auth-639f2",
-    storageBucket: "react-video-auth-639f2.appspot.com",
-    messagingSenderId: "636448339185",
-    appId: "1:636448339185:web:d62dd6954405af22846550",
-    measurementId: "G-NRVMVV6NYC"
+    databaseURL: "https://react-video-auth-XXXXXXXXXXX-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "react-video-auth-XXXXXXXXXXX",
+    storageBucket: "react-video-auth-XXXXXXXXXXX.appspot.com",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXXXXXXXXX"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
